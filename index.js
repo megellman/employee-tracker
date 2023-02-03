6889c2ac168b6b92a3fb9b0e59ad2c35749d6010
Megan Ellman <meganlellman@gmail.com>
2023-02-02 22:58:40 -0800

    made the addrole function async

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ function prompt() {
                 addEmployee();
             } else if (response.homepage === 'Update an employee role') {
                 updateEmployee();
-            } else if (response.homepage === 'I\'m done'){
+            } else if (response.homepage === 'I\'m done') {
                 console.log('Goodbye!');
                 process.exit();
             }
@@ -99,41 +99,36 @@ async function getDepartments() {
 
 // Creates a new role
 async function addRole() {
-    getDepartments().then(response => {
-        inquirer.prompt(
-            [{
-                type: "input",
-                input: "Enter the name of the role you would like to add",
-                name: "role",
-            },
-            {
-                type: "number",
-                input: "Enter the salary for this role",
-                name: "salary",
-            },
-            {
-                type: "list",
-                input: "Enter the name of the department this role falls under",
-                choices: response.map((roleData) => {
-                    return roleData.name
-                }),
-                name: "department",
-            }]
-        )
-            .then(answer => {
-                const selectedRole = response.filter(role => {
-                    return role.name == answer.department
-                })
-                db.query("INSERT INTO role(title, salary, department_id) VALUES (?, ?, ?)", [answer.role, answer.salary, selectedRole[0].id], function (err, result) {
-                    if (err) {
-                        throw err;
-                    } else {
-                        console.log(`${answer.role} has been added!`);
-                        prompt();
-                    }
-                });
-            })
-    })
+    const response = await getDepartments()
+    const answer = await inquirer.prompt(
+        [{
+            type: "input",
+            message: "Enter the name of the role you would like to add",
+            name: "role",
+        },
+        {
+            type: "number",
+            message: "Enter the salary for this role",
+            name: "salary",
+        },
+        {
+            type: "list",
+            message: "Enter the name of the department this role falls under",
+            choices: response.map((roleData) => {
+                return roleData.name
+            }),
+            name: "department",
+        }]
+    )
+    const selectedRole = await response.filter(role => role.name == answer.department)
+    db.query("INSERT INTO role(title, salary, department_id) VALUES (?, ?, ?)", [answer.role, answer.salary, selectedRole[0].id], function (err, result) {
+        if (err) {
+            throw err;
+        } else {
+            console.log(`${answer.role} has been added!`);
+            prompt();
+        }
+    });
 };
 
 // Get manager ids and names

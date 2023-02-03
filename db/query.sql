356413727b5b271dbf4a0e16efa4ceff021f7ae4
Megan Ellman <meganlellman@gmail.com>
2023-02-02 20:35:33 -0800

    created queries for each function

--- db/query.sql
+++ db/query.sql
@@ -1,13 +1,13 @@
--- I am presented with a formatted table showing department names and department ids
+-- Department names and department ids
 SELECT * FROM department;
 
--- I am presented with the job title, role id, the department that role belongs to, and the salary for that role
+-- Job title, role id, the department that role belongs to, and the salary for that role
 SELECT role.title, role.id, department.name, role.salary 
 FROM role
 Left JOIN department
 ON role.department_id = department.id;
 
--- I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
+-- Employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
 SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name, role.salary, employee.manager_id
 FROM department
 JOIN role
@@ -15,12 +15,24 @@ ON department.id = role.department_id
 JOIN employee
 ON employee.role_id = role.id;
 
+-- Add a department
+INSERT INTO department(name) VALUES (?);
 
-INSERT INTO department(name)
-VALUES (?)
+-- Add a role 
+INSERT INTO role(title, salary, department_id) VALUES (?, ?, ?);
 
-INSERT INTO role(title, salary, department)
-VALUES (?)
+-- Manager first and last name and id
+SELECT employee.id, CONCAT (first_name, ' ', last_name) AS name FROM employee;
 
-INSERT INTO role(first_name, last_name, role_id, manager_id)
-VALUES (?)
+-- Role id and title
+SELECT role.id, title FROM role;
+
+-- Add an employee
+INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?);
+
+-- Get employee first and last name and id and role id
+SELECT CONCAT(first_name, ' ', last_name) AS name, id, role_id FROM employee;
+
+-- Add a new employee
+UPDATE employee SET role_id = (?) WHERE id = (?);
+
